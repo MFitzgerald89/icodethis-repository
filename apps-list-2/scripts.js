1d032d9ec5a3af6bd9bb456868e2534419b5a468
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-08-07 22:11:53 -0400

    completes styling to best of ability for submission

--- apps-list-2/scripts.js
+++ apps-list-2/scripts.js
@@ -1,19 +1,35 @@
-const pageData = {
-  name: "Netflix",
-  company: "Netflix, Inc.",
-  rating: 5,
-};
-
-document.getElementById("pageName").innerText = pageData.name;
-document.getElementById("pageCompany").innerText = pageData.company;
+const pageData = [
+  {
+    name: "30 Day Fit",
+    pageRating: 3,
+  },
+  {
+    name: "Sandstorm",
+    pageRating: 4,
+  },
+];
 
 const ratingContainer = document.getElementById("pageRating");
-for (let i = 1; i <= 5; i++) {
-  const star = document.createElement("div");
-  if (i <= pageData.rating) {
-    star.className = "star";
-  } else {
-    star.className = "blank-star";
+
+pageData.forEach(({ name, pageRating }) => {
+  const pageContainer = document.createElement("div");
+
+  const pageNameElement = document.createElement("div");
+  pageNameElement.innerText = "Name: " + name;
+  pageContainer.appendChild(pageNameElement);
+
+  const ratingStarsElement = document.createElement("div");
+  ratingStarsElement.innerHTML = getRatingStarsHTML(pageRating);
+  pageContainer.appendChild(ratingStarsElement);
+
+  document.getElementById("pageList").appendChild(pageContainer);
+});
+
+function getRatingStarsHTML(rating) {
+  let starsHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const star = i <= rating ? "★" : "☆";
+    starsHTML += `<span class="${i <= rating ? "star" : "blank-star"}">${star}</span>`;
   }
-  ratingContainer.appendChild(star);
+  return starsHTML;
 }
